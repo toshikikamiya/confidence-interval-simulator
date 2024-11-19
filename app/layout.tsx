821387368713cd4d410b0f6e808449a120149309
T.K. <59132300+toshikikamiya@users.noreply.github.com>
2024-11-19 14:33:44 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen bg-background">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
