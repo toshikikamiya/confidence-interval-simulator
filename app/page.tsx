ee47ab9b2a16a527455d5d8bea31c520700e65b5
T.K. <59132300+toshikikamiya@users.noreply.github.com>
2024-11-19 13:01:35 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConfidenceIntervalSimulator from '../components/ConfidenceIntervalSimulator';
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <ConfidenceIntervalSimulator />
+    </main>
+  );
+}
